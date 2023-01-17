710dc09a7324281f308083a26d20e731db32524b
bezbrain <belloquadriolawale@gmail.com>
2023-01-17 00:08:21 -0800

    Step 2: Plan section completed

--- nav_through_steps.js
+++ nav_through_steps.js
@@ -2,21 +2,40 @@
 // OnClick of "Next Step" btn, move to the next step
 // =====>>>
 const stepsBtn = document.querySelectorAll(".steps-btn");
+const selectAPlan = document.querySelector(".select-plan");
 
 
-stepsBtn.forEach((eachStepBtn) => {
+let selectedPlan = "";
+
+stepsBtn.forEach((eachStepBtn, j) => {
     eachStepBtn.onclick = (e) => {
         let parent = e.currentTarget.parentElement;
         let parentNextSibling = e.currentTarget.parentElement.nextElementSibling;
         
+        // If Statement to stop moving to next step if no plan is clicked
+        if(j === 1) {
+            if(!selectedPlan) {
+                parent.style.display = "block";
+                parentNextSibling.style.display = "none";
+                selectAPlan.textContent = "please, select a plan";
+                
+            }
+            else {
+                parent.style.display = "none";
+                parentNextSibling.style.display = "block";
+            }
+        }
+        else {
+            parent.style.display = "none";
+            parentNextSibling.style.display = "block";
+        }
         
-        parent.style.display = "none";
-        parentNextSibling.style.display = "block";
-
         // emptySpace();
+        finishingUp();
+        addOns();
         circles();
 
-        // Using Tenary operation to display thank-you container in block
+        // Using Tenary operator to display thank-you container in block
         thankYou.style.display == "block" ? thankYou.style.display = "flex" : thankYou.style.display = "none";
     }
 });
@@ -71,7 +90,6 @@ function emptySpace() {
 
 
 
-
 // =====>>>
 // Select your plan
 // =====>>>
@@ -94,12 +112,15 @@ const getZIndexMonthly = window.getComputedStyle(monthlySwitcher);
 const getZIndexYearly = window.getComputedStyle(yearlySwitcher);
 
 // Toggling Monthly/Yearly
+let billType = "monthly";
+
 monthlySwitcher.onclick = () => {
     if(getZIndexMonthly.zIndex == "20") {
         monthlySwitcher.style.zIndex = "-20";
         yearlySwitcher.style.zIndex = "20";
         switchTextColorMonth.style.fontWeight = "200";
         switchTextColorYear.classList.add("text-color-year");
+        billType = "yearly";
 
         yearText.forEach((eachYearText, j) => {
             eachYearText.textContent = "2 months free";
@@ -109,6 +130,7 @@ monthlySwitcher.onclick = () => {
         advAmt.textContent = "$120/yr";
         proAmt.textContent = "$150/yr";
     }
+    // console.log(billType);
 }
 
 yearlySwitcher.onclick = () => {
@@ -117,6 +139,7 @@ yearlySwitcher.onclick = () => {
         monthlySwitcher.style.zIndex = "20";
         switchTextColorYear.classList.remove("text-color-year");
         switchTextColorMonth.style.fontWeight = "800";
+        billType = "monthly";
 
         yearText.forEach((eachYearText) => {
             eachYearText.textContent = "";
@@ -124,21 +147,162 @@ yearlySwitcher.onclick = () => {
 
         arcadeAmt.textContent = "$9/mo";
         advAmt.textContent = "$12/mo";
-        proAmt.textContent = "$15/mo";
-        
+        proAmt.textContent = "$15/mo";  
     }
+    // console.log(billType);
 }
 
 // Picking preferred plan
+
 plans.forEach((eachPlan) => {
     eachPlan.onclick = (e) => {
         plans.forEach((defaultBorder) => {
             defaultBorder.style.border = "2px solid hsl(231, 11%, 63%)";
         });
-        
         e.currentTarget.style.border = "4px solid hsl(231, 20%, 54%)";
+        selectedPlan = eachPlan.children[1].children[0].textContent;
+        selectAPlan.textContent = "";
     }
 });
+
+
+
+
+// =====>>>
+// Pick add-ons
+// =====>>>
+const addOnArr = [
+    {
+        name: "Online service",
+        duration: function() {
+            if(billType == "monthly") {
+                return "+$1/mo";
+            }
+            else {
+                return "+10/yr";
+            }
+        }
+    },
+
+    {
+        name: "Large storage",
+        duration: function() {
+            if(billType == "monthly") {
+                return "+$2/mo";
+            }
+            else {
+                return "+20/yr";
+            }
+        }
+    },
+
+    {
+        name: "Customizable profile",
+        duration: function() {
+            if(billType == "monthly") {
+                return "+$2/mo";
+            }
+            else {
+                return "+20/yr";
+            }
+        }
+    },
+];
+
+
+const price = document.querySelectorAll(".price");
+
+// Function to put the amounts for respective duration
+function addOns() {
+    price.forEach((eachPrice, j) => {
+        eachPrice.textContent = addOnArr[j].duration();
+    });
+}
+
+
+
+
+// =====>>>
+// Finishing Up
+// =====>>>
+const planArr = [
+    {
+        name: "Arcade",
+        duration: function() {
+            if(billType == "monthly") {
+                return "$9/mo";
+            }
+            else {
+                return "$90/yr";
+            }
+        },
+        category: "arc",
+    },
+
+    {
+        name: "Advanced",
+        duration: function() {
+            if(billType == "monthly") {
+                return "$12/mo";
+            }
+            else {
+                return "$120/yr";
+            }
+        },
+        category: "adv",
+    },
+
+    {
+        name: "Pro",
+        duration: function() {
+            if(billType == "monthly") {
+                return "$15/mo";
+            }
+            else {
+                return "$150/yr";
+            }
+        },
+        category: "pro",
+    }
+];
+
+const finishContent = document.querySelector(".finish-content");
+const arcadePara = finishContent.querySelector("p");
+
+const amt = finishContent.nextElementSibling;
+
+function finishingUp() {
+    if(selectedPlan == "Arcade") {
+            // console.log("I am Arcade");
+            arcadePara.textContent = planArr[0].name;
+            amt.textContent = planArr[0].duration();
+        }
+        else if(selectedPlan == "Advanced") {
+            // console.log("I am Advance");
+            arcadePara.textContent = planArr[1].name;
+            amt.textContent = planArr[1].duration();
+        }
+        else {
+            // console.log("I am Pro");
+            arcadePara.textContent = planArr[2].name;
+            amt.textContent = planArr[2].duration();
+    }
+}
+
+
+const checkBox = document.querySelectorAll(".checkb");
+console.log(checkBox);
+
+checkBox.forEach((eachCheckBox) => {
+    eachCheckBox.onclick = (e) => {
+        console.log(e.currentTarget);
+    }
+})
+
+
+
+
+
 
 
 
